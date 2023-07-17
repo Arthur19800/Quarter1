c95631854b9641bc33963171a40fbed062b9af89
Arthur19800 <info3746@gmail.com>
2023-07-17 09:05:49 +0300

    Input array size added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задача : Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. ");
+Console.Write("input array size m: ");
+int m;
+
+do
+{
+    m = Convert.ToInt32(Console.ReadLine()); 
+    if  (m == 0) Console.WriteLine("Wrong array size");
+    else Console.WriteLine("Correct array size! Input array elememts: ");
+} while (m == 0);
